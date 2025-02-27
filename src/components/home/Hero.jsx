8749c632cbd7cc116a601a6f5a58190f73cc68ef
AhmedAbdullah125 <ahmednabeeh98@gmail.com>
2025-02-27 13:49:01 +0200

    uodate

--- src/components/home/Hero.jsx
+++ src/components/home/Hero.jsx
@@ -1,16 +1,18 @@
 'use client'
 import React, { useEffect, useState } from 'react';
-import bg from '/public/HeroBg.jpg'
 import Link from 'next/link';
-import axios from 'axios';
-import { API_BASE_URL } from '@/lib/apiConfig';
-import Loading from '@/app/loading';
+
 import { Autoplay, Navigation, Pagination } from "swiper/modules";
 import { Swiper, SwiperSlide } from "swiper/react";
 import "swiper/css";
 import "swiper/css/navigation";
 import "swiper/css/pagination";
+import axios from 'axios';
+import { API_BASE_URL } from '@/lib/apiConfig';
+import Loading from '@/app/loading';
+
 export default function Hero() {
+    const [muted, setMuted] = useState(true);
     const [loading, setLoading] = useState(true); // State for loading indicator
     const [data, setData] = useState(null);
     const [error, setError] = useState(null);
@@ -34,61 +36,69 @@ export default function Hero() {
             });
 
     }, []);  // Run this effect whenever the `language` changes
+    console.log(data);
+
     return (
+        <div className="hero">
+            <i className={`fa-solid mute-icon ${muted ? " fa-volume-low" : "fa-volume-xmark"}`} onClick={() => setMuted(!muted)}></i>
 
-
-        <div>
+            <video
+                width="320"
+                height="240"
+                autoPlay
+                loop
+                muted={muted}
+            >
+                <source src='/video1.mp4' type="video/mp4" />
+                <source src='/video1.mp4' type="video/ogg" />
+                Your browser does not support the video tag.
+            </video>
             {
                 loading ? <Loading /> :
-                    <Swiper
-                        slidesPerView={3.1}
-                        spaceBetween={24}
-                        autoplay={false}
-                        dir={'rtl'}
-                        loop={true}
-                        modules={[Autoplay, Navigation, Pagination]}
-                        breakpoints={{
-                            1400: {
-                                slidesPerView: 1,
-                            },
-                            1100: {
-                                slidesPerView: 1,
-                            },
-                            767: {
-                                slidesPerView: 1,
-                            },
-                            640: {
-                                slidesPerView: 1,
-                                autoplay: false,
-                                spaceBetween: 16
-                            },
-                            100: {
-                                slidesPerView: 1,
-                                autoplay: false,
-                                spaceBetween: 16
-                            }
-                        }}
-                    >
-                        {data.map((item, index) =>
-                            <SwiperSlide key={index}>
-                                <div className="hero" style={{ backgroundImage: `url(${item.image})`, backgroundRepeat: 'no-repeat', backgroundSize: 'cover', backgroundPosition: 'center center' }}>
-                                    <div className="overlay">
-                                        <div className="heading">
-                                            <h1>{item.title}</h1>
-                                            <p>{item.description}</p>
-                                            <div className="links">
-                                                <Link href="/#services" className='sec-link' >تعرف علي خدماتنا </Link>
-                                            </div>
+                    <div className="overlay">
+                        <Swiper
+                            slidesPerView={3.1}
+                            spaceBetween={24}
+                            autoplay={false}
+                            dir={'rtl'}
+                            loop={true}
+                            modules={[Autoplay, Navigation, Pagination]}
+                            breakpoints={{
+                                1400: {
+                                    slidesPerView: 1,
+                                },
+                                1100: {
+                                    slidesPerView: 1,
+                                },
+                                767: {
+                                    slidesPerView: 1,
+                                },
+                                640: {
+                                    slidesPerView: 1,
+                                    autoplay: false,
+                                    spaceBetween: 16
+                                },
+                                100: {
+                                    slidesPerView: 1,
+                                    autoplay: false,
+                                    spaceBetween: 16
+                                }
+                            }}
+                        >
+                            {data.map((item, index) =>
+                                <SwiperSlide key={index}>
+                                    <div className="heading">
+                                        <h1>{item.title}</h1>
+                                        <p>{item.description}</p>
+                                        <div className="links">
+                                            <Link href="/#services" className='sec-link' >تعرف علي خدماتنا </Link>
                                         </div>
                                     </div>
-                                </div>
-                            </SwiperSlide>
-                        )}
-                    </Swiper>
+                                </SwiperSlide>
+                            )}
+                        </Swiper>
+                    </div>
             }
-
         </div>
-
-
     );
 }
